a00e6abe2720c1e46c3428e65cb001779b3ef04a
Eduart2021 <zeedymarina@yahoo.com>
2022-07-11 00:21:48 -0400

    thre

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer')
 
 
-test('launch chromium', async () =>{
+test.skip('launch chromium', async () =>{
     const browser = await puppeteer.launch({ //object -> {}
        // headless: false,
         slowMo: 60,
         //args:['--window-size=1080, 950']
     })
-    let url = 'file:///C:/Users/zeedy/OneDrive/Desktop/unitTestJS/puppeteer/src/index.html'
+    let url = 'file:///C:/Users/zeedy/OneDrive/Desktop/unitTestJS/puppet/src/index.html'
     let url2 = 'http://127.0.0.1:5500/puppeteer/src/index.html'
     const page = await browser.newPage()
     await page.goto(url)
@@ -17,8 +17,8 @@ test('launch chromium', async () =>{
     // age
     await page.click('input#age')
     await page.type('input#age', '21')
-    await page.keyboard.press('Enter')
-    //await page.click('#btnAddUser')
+
+    await page.click('#btnAddUser')
     const finalText = await page.$eval('.user-list', el => el.textContent )
     let aList =[];
     if(finalText) aList.push(finalText)
